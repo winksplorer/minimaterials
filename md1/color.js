a26bc1239d7a614713956bd7a0291b8c90cea1a4
winksplorer <wink@winksplorer.net>
2025-12-14 20:44:04 -0800

    md1/color: refactoring and format change

--- md1/color.js
+++ md1/color.js
@@ -1,39 +1,29 @@
 window.md1  = window.md1 || {};
-md1.roles = ['primary', 'secondary']
-md1.rgb2hex = ([r,g,b]) => '#' + [r,g,b].map(x => x.toString(16).padStart(2, '0')).join('');
+md1.roles = ['primary', 'secondary'];
 md1.shadeId = i => i < 10 ? (i * 100).toString() : 'a' + [100,200,400,700][i-10];
 md1.getColorClass = (joined, role) => (joined.match(new RegExp(`\\bmm-${role}-([^\\s]+)`)) || [])[1] || null;
-md1.shouldUseWhiteText = hex => {
-    const [r, g, b] = hex.match(/\w\w/g).map(x => parseInt(x, 16));
-    return (r * 299 + g * 587 + b * 114) / 1000 < 128;
-};
+md1.cssHsl = (h, s, l) => `hsl(${h}, ${s}%, ${l}%)`;
 
 md1.encodedColors = `
-A3JlZA7/6+7/zdLvmprlc3PvU1D0QzblOTXTLy/GKCi3HBz/ioD/UlL/F0TVAAAEcGluaw785Oz4u9D0j7HwYpLsQHrpHmPYG2DCGFutFFeIDk//gKv/QIH1
-AFfFEWIGcHVycGxlDvPl9eG+586T2LpoyKtHvJwnsI4kqnsfomobmkoUjOqA/OBA+9UA+aoA/wtkZWVwLXB1cnBsZQ7t5/bRxOmznduVdc1+V8JnOrdeNbFR
-LahFJ6AxG5KziP98Tf9lH/9iAOoGaW5kaWdvDujq9sXK6Z+o2nmGy1xrwD9RtTlJqzA/nyg1kxojfoye/1Nt/j1a/jBP/gRibHVlDuPy/bve+5DK+WS19kKl
-9SGW8x6I5Rl20hVlwA1HoYKx/0SK/yl5/yli/wpsaWdodC1ibHVlDuH1/rPl/IHU+k/D9ym29gOp9AOb5QKI0QJ3vQFXm4DY/0DE/wCw/wCR6gRjeWFuDuD3
-+rLr8oDe6k3Q4SbG2gC81ACswQCXpwCDjwBgZIT//xj//wDl/wC41AR0ZWFsDuDy8bLf24DLxE22rCammgCWiACJewB5awBpXABNQKf/62T/2h3ptgC/pQVn
-cmVlbg7o9enI5sml1qeBx4Rmu2pMr1BDoEc4jjwufTIbXiC59spp8K4A5nYAyFMLbGlnaHQtZ3JlZW4O8fjp3O3IxeGlrtWBnMxli8NKfLNCaJ84VYsvM2ke
-zP+Qsv9Zdv8DZN0XBGxpbWUO+fvn8PTD5u6c3Od11OFXzdw5wMozr7Qrnp0kgncX9P+B7v9Bxv8AruoABnllbGxvdw7//ef/+cT/9Z3/8Xb/7lj/6zv92DX7
-wC35qCX1fxf//43//wD/6gD/1gAFYW1iZXIO//jh/+yz/+CC/9VP/8oo/8EH/7MA/6AA/48A/28A/+V//9dA/8QA/6sABm9yYW5nZQ7/8+D/4LL/zID/t03/
-pyb/mAD7jAD1fADvbADmUQD/0YD/q0D/kQD/bQALZGVlcC1vcmFuZ2UO++nn/8y8/6uR/4pl/3BD/1ci9FEe5koZ2EMVvzYM/56A/25A/z0A3SwABWJyb3du
-Cu/r6dfMyLyqpKGIf41uY3lVSG1MQV1AN040Lj4nIwRncmF5Cvr6+vX19e7u7uDg4L29vZ6ennV1dWFhYUJCQiEhIQlibHVlLWdyYXkK7O/xz9jcsL7FkKSu
-eJCcYH2LVG56RVpkN0dPJjI4`.trim();
+    A3JlZA4AV2BaTUM/OjczLylLQjcqBHBpbmsO8VJeVUxCOzQwKyYdSz8wKgZwdXJwbGUOz0NdU0c8MyooJiMfSz4xMgtkZWVwLXB1cnBsZQ66OF5USj83Ly0qJyJNQTgu
+    BmluZGlnbw6kNF5USkA4MC0pJR5NQj47BGJsdWUOlVpeVk1EPTYzLioiSz86OgpsaWdodC1ibHVlDo1iXlVKQDgwLSklH0s/Mi4EY3lhbg6FZF1SRzsyKiYhHBRMNzIq
+    BHRlYWwOe2RbT0EzKB0bGBUPU0YzJQVncmVlbg5XKl5USkA5MS0nIhhVRC0nC2xpZ2h0LWdyZWVuDj8zXlZMQzw1MCokGk5DMzAEbGltZQ4vRl9WTUQ9NjIsJh5LPzIu
+    BnllbGxvdw4mZF9YUUlDPjw6ODVOMjIyBWFtYmVyDiBkXlVLQTozMjIyMks/MjIGb3JhbmdlDhlkXlVLQTkyMTAvLUs/MjILZGVlcC1vcmFuZ2UOCmRfV05GPzk2Mi4o
+    Sz8yKwVicm93bgoLFl1RRTgvJiIdGBMEZ3JheQoAAGJgXVhKPi4mGg0JYmx1ZS1ncmF5Co4RXlRJPjYuKCEaEg==`.trim();
 
 md1.colors = md1.colors || {};
-md1.updatePalette = function(role) {
+md1.updatePalette = (role) => {
     // check for valid role, and get the requested family
-    if (!md1.roles.includes(role)) throw new Error(`minimaterials: ${role}: role does not exist`);
+    if (!md1.roles.includes(role)) throw new Error(`mm-md1: ${role}: role does not exist`);
     let family = md1.getColorClass(document.body.className, role);
 
     // check if the family exists
-    if (!(family in md1.colors)) throw new Error(`minimaterials: ${role}: ${family}: color family does not exist`);
+    if (!(family in md1.colors)) throw new Error(`mm-md1: ${role}: ${family}: color family does not exist`);
 
     // set css vars accordingly
     Object.keys(md1.colors[family]).forEach(shade => {
-        document.body.style.setProperty(`--mm-${role}-${shade}`, md1.colors[family][shade]);
-        document.body.style.setProperty(`--mm-on-${role}-${shade}`, md1.shouldUseWhiteText(md1.colors[family][shade]) ? '#fff' : "#000");
+        document.body.style.setProperty(`--mm-${role}-${shade}`, md1.colors[family][shade][0]);
+        document.body.style.setProperty(`--mm-on-${role}-${shade}`, md1.colors[family][shade][1] ? '#fff' : "#000");
     });
 }
 
@@ -41,30 +31,31 @@ md1.bodyObserver = new MutationObserver(muts => {
     muts.forEach(mut => 
         md1.roles.forEach(role => 
             md1.getColorClass(mut.oldValue, role) !== md1.getColorClass(mut.target.className, role) 
-            ? md1.updatePalette(role) : null)
+                && md1.updatePalette(role)
+        )
     );
 });
 
-(function(){
+(() => {
     const payload = Uint8Array.from(atob(md1.encodedColors), c => c.charCodeAt(0));
     const view = new DataView(payload.buffer, payload.byteOffset, payload.byteLength);
+    const dec = new TextDecoder();
 
     // initial load - "cache" all color data in object notation
     for (let off = 0; off < payload.length-1;) {
-        // get colorfamily name (and it's length)
-        const familyNameLength = view.getUint8(off++);
-        const familyName = (new TextDecoder()).decode(payload.subarray(off, off+familyNameLength));
-        off += familyNameLength;
+        const familyNameLength = view.getUint8(off++), // family name length [number]
+            familyName = dec.decode(payload.subarray(off, off+=familyNameLength)), // family name [string]
+            familyShades = view.getUint8(off++), // amount of shades in family [number]
+            h = view.getUint8(off++) * 360 / 256, // median H [number]
+            s = view.getUint8(off++); // median S [number]
 
         md1.colors[familyName] = md1.colors[familyName] || {};
-
-        // go through shades and repack them into string hex (they were encoded as literal hex)
-        for (let shade = 0, familyShades = view.getUint8(off++); shade < familyShades; shade++)
-            md1.colors[familyName][md1.shadeId(shade)] = md1.rgb2hex([
-                view.getUint8(off++), // r
-                view.getUint8(off++), // g
-                view.getUint8(off++)  // b
-            ]);
+        
+        for (let shade = 0; shade < familyShades; shade++) {
+            // store css color string alongside "should use white text"
+            const l = view.getUint8(off++);
+            md1.colors[familyName][md1.shadeId(shade)] = [md1.cssHsl(h,s,l), (l + 0.25*s * Math.cos((h-120)*Math.PI / 180)) < 50];
+        }
     }
 
     // set up observer
--- tools/color-encoder.js
+++ tools/color-encoder.js
@@ -6,22 +6,57 @@ let colors  = {};
 const bytes = [];
 
 const hex2rgb = hex => hex.match(/\w\w/g).map(x => parseInt(x, 16));
+const rgb2hsl = ([r, g, b]) => {
+    r /= 255; g /= 255; b /= 255;
+    let max = Math.max(r, g, b);
+    let min = Math.min(r, g, b);
+    let d = max - min;
+    let h;
+    if (d === 0) h = 0;
+    else if (max === r) h = (g - b) / d % 6;
+    else if (max === g) h = (b - r) / d + 2;
+    else if (max === b) h = (r - g) / d + 4;
+    let l = (min + max) / 2;
+    let s = d === 0 ? 0 : d / (1 - Math.abs(2 * l - 1));
+    return [(h * 60).toFixed(2), s.toFixed(2), l.toFixed(2)];
+}
+
+const median = (values)  => {
+    if (!values.length) return 0;
+    values = [...values].sort((a, b) => a - b);
+    const half = Math.floor(values.length / 2);
+    return (values.length % 2 ? values[half] : (values[half - 1] + values[half]) / 2);
+}
+
+
 
 for (const [,family,shade,hex] of vars) {
     colors[family] ??= {};
-    colors[family][shade] = hex2rgb(hex);
+    colors[family][shade] = rgb2hsl(hex2rgb(hex));
 }
 
 for (const family of Object.keys(colors)) {
+    // get median h and s
+    let hVals = [], sVals = [];
+    for (const shade of Object.keys(colors[family])) {
+        hVals.push(parseFloat(colors[family][shade][0]));
+        sVals.push(parseFloat(colors[family][shade][1]));
+    }
+
+    const h8 = Math.round(median(hVals) * 256 / 360) & 0xFF // decode: hue = h8 * 360 / 256
+    const s8 = Math.round(median(sVals)*100) // decode: s = s8/100
+
     bytes.push(
         family.length, // characters in color family name
         ...enc.encode(family), // color family name encoded in utf-8
-        Object.keys(colors[family]).length // the amount of shades in the color family
+        Object.keys(colors[family]).length, // the amount of shades in the color family
+        h8, // average h (hsl)
+        s8 // average s (hsl)
     );
 
-    for (const shade of Object.keys(colors[family])) {
-        for (const channel of colors[family][shade]) bytes.push(channel);
-    }
+    for (const shade of Object.keys(colors[family])) bytes.push(Math.round(colors[family][shade][2]*100));
 }
+
+console.log(bytes);
 
 fs.writeFileSync('colors.bin', Uint8Array.from(bytes), null);
